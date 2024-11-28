7cbde17ecae02d8cf11088d9b3950d8aa2c5be3e
Priyanshu Prasad Gupta <priyanshoon.pg@gmail.com>
2024-11-28 16:57:33 +0530

    initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{
+			"message": "Hello, World!",
+		})
+	})
+	e.Logger.Fatal(e.Start(":6969"))
+}
